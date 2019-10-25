51c54e5b54c9d9cab867220c969975d482358c7d
Vitor Maia <vitormaia@viitra.com>
2019-10-25 20:09:43 -0300

    add container

---
+++ src/components/Graph/Node/Node.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Node.css';
+import PropTypes from 'prop-types';
+
+
+const Node = (props) => {
+	const style = {
+
+	}
+	return (
+		<div className='Node'>
+
+		</div>
+	)
+}
+
+Node.propTypes = {
+	id: PropTypes.
+	color: PropTypes.string,
+	size: PropTypes.number,
+	x: PropTypes.number,
+	y: PropTypes.number
+}
+
+export default Node;
