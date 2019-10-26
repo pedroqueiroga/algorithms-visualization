f12aa231ff66d6509eb140ea66c0b639ce77c446
Vitor Maia <vitormaia@viitra.com>
2019-10-26 13:13:06 -0300

    add test graph display

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-import Graph from '../components/Graph/Graph';
+import Graph from './Graph/Graph';
 
 function App() {
+
   return (
     <div className="App">
-      <Graph nodes={[
-        {x: 50, y: 50, label: 'A'},
-        {x: 100, y: 100, label: 'B'},
-        {x: 50, y: 100, label: 'C'},
-        {x: 100, y: 50, label: 'D'}
-      ]} edges={[
-       { labelNode1: 'A',
-        labelNode2: 'B'},
-        { labelNode1: 'B',
-         labelNode2: 'C'},
-         { labelNode1: 'C',
-          labelNode2: 'D'}
-      ]}>
+      <Graph name="G1">
 
       </Graph>
     </div>
