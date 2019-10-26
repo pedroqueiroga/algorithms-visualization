d42c76ea5fcdc55f94603c2a1e348d945c3037a9
Vitor Maia <vitormaia@viitra.com>
2019-10-26 11:13:51 -0300

    adapting Node module to pattern and improving style

--- src/components/Graph/Node/Node.js
+++ src/components/Graph/Node/Node.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import './Node.css';
+import classes from './Node.module.css';
 import PropTypes from 'prop-types';
 
 const node = (props) => {
 	let { id, color, size, x, y, label } = props;
 	if (!size) size = 50;
 	const dynamicStyle = {
-		backgroundColor: color ? color : 'black',
+		backgroundColor: color ? color : 'rgba(0.6, 0.6, 0.6, 0.6)',
 		width: size,
 		height: size,
 		left: x - size/2,
 		top: y - size/2
 	}
 	return (
-		<div className='Node' style={dynamicStyle} key={id}>
+		<div className={classes.Node} style={dynamicStyle} key={id}>
 			{label}
 		</div>
 	)
